5057c9d08c106373211e828a9876c5c1785996dc
Alessandro <ale54@live.it>
2021-08-18 13:53:23 +0200

    aggiornato 6.5.1 phpmailer, sicurezza contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,6 @@
 <?php
   /**
   * @see https://bootstrapmade.com/php-email-form/
-  * TODO sicurezza
   */
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
@@ -17,6 +16,7 @@
   $contact->isSMTP();
   $contact->Host = $INFO['host'];
   $contact->Port = $INFO['port'];
+  $contact->CharSet = PHPMailer::CHARSET_UTF8;
   $contact->SMTPAuth = true;
   $contact->Username = $INFO['username']; 
   $contact->Password = $INFO['password'];
@@ -25,16 +25,24 @@
   $contact->SMTPDebug = 0;
 
   $contact->setFrom($INFO['to_mail'], $INFO['to_name']);
-  $contact->Subject = $_POST['subject'];
+  $name = substr(strip_tags($_POST['name']), 0, 255);
+  $subject = substr(strip_tags($_POST['subject']), 0, 255);
+  $email = PHPMailer::validateAddress($_POST['email']) ? $_POST['email'] : '';
+  $plainMessage = htmlspecialchars( $_POST['message'], ENT_QUOTES | ENT_DISALLOWED, 'UTF-8', FALSE );
+  $message = nl2br( $message );
+  $contact->Subject = $subject;
 
   /* mail admin */
   $contact->addAddress($INFO['to_mail'], $INFO['to_name']);
-  $contact->AddReplyTo($_POST['email'], $_POST['name']);
+  $contact->AddReplyTo($email, $name);
   $contact->isHTML(true);
 
-  $contact->Body = "<h2>DA: {$_POST['name']}</h2> <p>Richiesta \"{$_POST['subject']}\"</p><br><br><p>{$_POST['message']}</p>";
-  $contact->AltBody = "DA {$_POST['name']} \n Richiesta \"{$_POST['subject']}\" \n\n {$_POST['message']}";
-  
+  $contact->Body = "<h2>DA: {$name}</h2> <p>Richiesta \"{$subject}\"</p><br><br><p>{$message}</p>";
+  $contact->AltBody = "DA {$name} \n Richiesta \"{$subject}\" \n\n {$plainMessage}";
+  $contact->AddCustomHeader( "X-Confirm-Reading-To: $email" );
+  $contact->AddCustomHeader( "Return-Receipt-To: $email" );
+  $contact->AddCustomHeader( "Disposition-Notification-To: $email" );
+
   try {
       $contact->send();
       $response = "OK";
@@ -46,16 +54,16 @@
   if(isset($_POST['bcc'])) {
     $contact->clearAddresses();
     $contact->clearReplyTos();
-    $contact->addAddress($_POST['email'], $_POST['name']);
+    $contact->addAddress($email, $name);
     $contact->AddReplyTo($INFO['to_mail'], $INFO['to_name']);
 
-    $contact->Body = "<h2>Gentile {$_POST['name']}</h2> <p>Ho ricevuto la sua richiesta \"{$_POST['subject']}\"</p><br><br><p>{$_POST['message']}</p>";
-    $contact->AltBody = "Gentile, {$_POST['name']} \n Ho ricevuto la sua richiesta \"{$_POST['subject']}\" \n\n {$_POST['message']}";
+    $contact->Body = "<h2>Gentile {$name}</h2> <p>Ho ricevuto la sua richiesta \"{$subject}\"</p><br><br><p>{$message}</p>";
+    $contact->AltBody = "Gentile, {$name} \n Ho ricevuto la sua richiesta \"{$subject}\" \n\n {$plainMessage}";
     
     try {
       $contact->send();
     } catch (Exception $e) {
-      $response .= "Mailer Error ({$_POST['email']}) {$contact->ErrorInfo}\n";
+      $response .= "Mailer Error ({$email}) {$contact->ErrorInfo}\n";
     }
   }
   $contact->smtpClose();
