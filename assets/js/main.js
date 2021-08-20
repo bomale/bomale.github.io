cd77d94bb5a3929573efde76514193de44b41ddd
Alessandro <ale54@live.it>
2021-08-20 23:40:05 +0200

    performance mobile hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -263,114 +263,26 @@
 
 })();
 
-/**
- * @license Asparagus v1.0
- * (c) 2013 Form5 http://form5.is
- * License: MIT
- */
-(function() {
-  var lastScrollY = 0,
-      ticking = false,
-      bgElm = document.getElementById('hero'),
-      speedDivider = 1.5;
+var options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: [...Array(100)].map((_, i) => 0 + i * .01)
+},
+onIntersect = (entries) => {
+  entries
+  .filter(entry => entry.isIntersecting)
+  .forEach((entry) => {
+      var scrollY = window.scrollY,
+        height = entry.target.clientHeight,
+        opacityValue = (height - (scrollY > height ? height : scrollY)) / height;
+        
+      [...entry.target.children].forEach(function(elm){
+        elm.style.opacity = opacityValue;
+        elm.style.transitionDelay = '0s';
+        elm.style.transform = 'translateY('+ scrollY / 2 +'px)';
+      });
+  });
+},
+observer = new IntersectionObserver(onIntersect, options);
 
-  // Update background position
-  var updatePosition = function() {
-    var translateValue = lastScrollY / speedDivider,
-        scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-        height = bgElm.clientHeight,
-        opacityValue   = (height - scrollTop) * speedDivider / height;
-
-    // We don't want parallax to happen if scrollpos is below 0
-    if (translateValue < 0)
-      translateValue = 0;
-
-    translateY(bgElm, translateValue);
-    [...bgElm.children].forEach(function(elm){
-      elm.style.opacity = opacityValue;
-      elm.style.transitionDelay = '0.1s';
-      elm.style.marginTop = translateValue * speedDivider + 'px';
-    });
-
-    // Stop ticking
-    ticking = false;
-  };
-
-  // Translates an element on the Y axis using translate3d to ensure
-  // that the rendering is done by the GPU
-  var translateY = function(elm, value) {
-    var translate = 'translate3d(0px,' + value + 'px, 0px)';
-    document.documentElement.style.setProperty("--transform-bg", 'translate3d(0px,' + value + 'px, 0px)');
-    /*elm.style['-webkit-transform'] = translate;
-    elm.style['-moz-transform'] = translate;
-    elm.style['-ms-transform'] = translate;
-    elm.style['-o-transform'] = translate;
-    elm.style.transform = translate;*/
-  };
-
-  // This will limit the calculation of the background position to
-  // 60fps as well as blocking it from running multiple times at once
-  var requestTick = function() {
-    if (!ticking) {
-      window.requestAnimationFrame(updatePosition);
-      ticking = true;
-    }
-  };
-
-  // Update scroll value and request tick
-  var doScroll = function() {
-    lastScrollY = window.scrollY;
-    requestTick();
-  };
-
-  // Initialize on domready
-  (function() {
-    var loaded = 0;
-    var bootstrap = function() {
-      if (loaded) return;
-      loaded = 1;
-
-      rafPolyfill();
-      window.onscroll = doScroll;
-      window.onload = doScroll;
-    };
-
-    if ( document.readyState === 'complete' ) {
-      setTimeout( bootstrap );
-    } else {
-      document.addEventListener( 'DOMContentLoaded', bootstrap, false );
-      window.addEventListener( 'load', bootstrap, false );
-    }
-  })();
-
-  // RequestAnimationFrame polyfill for older browsers
-  var rafPolyfill = function() {
-    var lastTime, vendors, x;
-    lastTime = 0;
-    vendors = ["webkit", "moz"];
-    x = 0;
-    while (x < vendors.length && !window.requestAnimationFrame) {
-      window.requestAnimationFrame = window[vendors[x] + "RequestAnimationFrame"];
-      window.cancelAnimationFrame = window[vendors[x] + "CancelAnimationFrame"] || window[vendors[x] + "CancelRequestAnimationFrame"];
-      ++x;
-    }
-    if (!window.requestAnimationFrame) {
-      window.requestAnimationFrame = function(callback, element) {
-        var currTime, id, timeToCall;
-        currTime = new Date().getTime();
-        timeToCall = Math.max(0, 16 - (currTime - lastTime));
-        id = window.setTimeout(function() {
-          callback(currTime + timeToCall);
-        }, timeToCall);
-        lastTime = currTime + timeToCall;
-        return id;
-      };
-    }
-    if (!window.cancelAnimationFrame) {
-      window.cancelAnimationFrame = function(id) {
-        clearTimeout(id);
-      };
-    }
-  };
-
-}).call(this);
+observer.observe(document.getElementById('hero'));
